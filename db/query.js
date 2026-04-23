ca19773eab51402ac4640a4035aa199a7a4ed226
Kris Wong <74948841+uwuzumaki@users.noreply.github.com>
2026-04-22 23:22:03 -0400

    queries for finding user via email or id

--- db/query.js
+++ db/query.js
@@ -11,6 +11,24 @@ const register = async (email, username, password) => {
   return user;
 };
 
+const findUserEmail = async (email) => {
+  const user = await prisma.user.findUnique({
+    where: {
+      email,
+    },
+  });
+  return user;
+};
+
+const findUserID = async (id) => {
+  const user = await prisma.user.findUnique({
+    where: {
+      id,
+    },
+  });
+  return user;
+};
+
 const createMessage = async (content) => {
   const message = await prisma.message.create({
     data: {
@@ -22,5 +40,7 @@ const createMessage = async (content) => {
 
 export default {
   register,
+  findUserEmail,
+  findUserID,
   createMessage,
 };
