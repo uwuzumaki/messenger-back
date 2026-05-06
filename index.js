0055b4555b82d90f4be588d2d922d96074a78d67
Kris Wong <74948841+uwuzumaki@users.noreply.github.com>
2026-05-05 23:20:32 -0400

    temporarily disabled chat

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "*",
+    origin: "http://localhost:5173",
     credentials: true,
   }),
 );
@@ -25,32 +25,32 @@ app.use(passport.session());
 app.use("/register", router.registration);
 app.use("/authentication", router.authentication);
 
-const server = createServer(app);
-const io = new Server(server, {
-  cors: {
-    origin: ["http://localhost:5173"],
-    credentials: true,
-  },
-});
+// const server = createServer(app);
+// const io = new Server(server, {
+//   cors: {
+//     origin: ["http://localhost:5173"],
+//     credentials: true,
+//   },
+// });
 
 app.get("/", (req, res) => {
   return res.send(200);
 });
 
-io.on("connection", (socket) => {
-  socket.on("chat message", async (msg) => {
-    let result;
-    try {
-      result = await db.createMessage(msg);
-    } catch (error) {
-      return;
-    }
-    io.emit("chat message", msg);
-    console.log(msg);
-  });
-});
+// io.on("connection", (socket) => {
+//   socket.on("chat message", async (msg) => {
+//     let result;
+//     try {
+//       result = await db.createMessage(msg);
+//     } catch (error) {
+//       return;
+//     }
+//     io.emit("chat message", msg);
+//     console.log(msg);
+//   });
+// });
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log(`Server running on port: ${PORT}`);
 });
