967627c6a991764c0c0fdb72b20d917c4dbf180b
Kris Wong <74948841+uwuzumaki@users.noreply.github.com>
2026-05-13 01:23:23 -0400

    Added authentication

--- routers/authenticationRouter.js
+++ routers/authenticationRouter.js
@@ -3,8 +3,25 @@ import { Router } from "express";
 
 const router = Router();
 
-router.post("/", passport.authenticate("local"), (req, res) =>
-  res.send({ message: "authenticated" }),
+router.post("/login", passport.authenticate("local"), (req, res) =>
+  res.json({ user: req.user }),
 );
+
+router.get("/verify", (req, res) => {
+  if (req.isAuthenticated()) {
+    return res.json({ user: req.user });
+  }
+  res.status(401).json({ user: null });
+});
+
+router.get("/logout", (req, res, next) => {
+  req.logout((err) => {
+    if (err) return next(err);
+    req.session.destroy((err) => {
+      if (err) return next(err);
+      res.clearCookie;
+    });
+  });
+});
 
 export default router;
