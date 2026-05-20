0c52dfa053be85361057449bdb4a5885781a979c
Kris Wong <74948841+uwuzumaki@users.noreply.github.com>
2026-05-19 21:26:37 -0400

    Updated prisma

---
+++ prisma/migrations/20260520012621_connection_state/migration.sql
@@ -0,0 +1,17 @@
+/*
+  Warnings:
+
+  - A unique constraint covering the columns `[username]` on the table `User` will be added. If there are existing duplicate values, this will fail.
+  - Added the required column `offset` to the `Message` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `userId` to the `Message` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "Message" ADD COLUMN     "offset" TEXT NOT NULL,
+ADD COLUMN     "userId" TEXT NOT NULL;
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
+
+-- AddForeignKey
+ALTER TABLE "Message" ADD CONSTRAINT "Message_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
